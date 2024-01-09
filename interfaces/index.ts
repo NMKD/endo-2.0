c78dc9aaea978c0dd0e94c8db0fd290e7ed24106
NMKD <christina.kryazheva@yandex.ru>
2024-01-09 15:36:07 +0300

    added interfaces - data of props and types of props

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -3,6 +3,29 @@ import {
   IEndoscopeFormatTable,
   ICirclesOfUser,
 } from "./extension/prisma.ext";
+import {
+  IEndoscopesProps,
+  TNullNumber,
+  TNullString,
+  TFnToCleaning,
+  TFnToStore,
+  TFnToEndWashingMch,
+  TFnToEndWashingMl,
+  TFnToResearch,
+} from "./props/props";
 import { columns } from "./table/columns";
 
+// columns of table and extension prisma servises
 export { columns, IHistoryResearch, IEndoscopeFormatTable, ICirclesOfUser };
+// data of props
+export { IEndoscopesProps };
+// types of props
+export {
+  TNullNumber,
+  TNullString,
+  TFnToCleaning,
+  TFnToStore,
+  TFnToEndWashingMch,
+  TFnToEndWashingMl,
+  TFnToResearch,
+};
---
+++ interfaces/props/props.ts
@@ -0,0 +1,29 @@
+import { Circle } from "@prisma/client";
+import { ICirclesOfUser } from "..";
+
+// types for data
+export type TNullNumber = null | number;
+export type TNullString = null | string;
+export type TFnToCleaning = (pcId: TNullString) => Promise<Circle | undefined>;
+export type TFnToStore = (id: TNullString) => Promise<void>;
+export type TFnToEndWashingMch = (
+  id: TNullString,
+  idp: TNullNumber
+) => Promise<Circle | undefined>;
+export type TFnToEndWashingMl = (
+  id: TNullString,
+  idp: TNullNumber
+) => Promise<Circle | undefined>;
+export type TFnToResearch = (
+  id: TNullNumber,
+  userId: TNullNumber
+) => Promise<Circle | undefined>;
+
+export interface IEndoscopesProps {
+  user: ICirclesOfUser;
+  toCleaning: TFnToCleaning;
+  toStore: TFnToStore;
+  toEndWashingMch: TFnToEndWashingMch;
+  toEndWashingMl: TFnToEndWashingMl;
+  toResearch: TFnToResearch;
+}
